1c8714d8dc2d0a220d5af99adf53b0da727fa179
Sayed Arif Uddin Pallab <47639147+sayedarifuddin@users.noreply.github.com>
2021-11-15 22:46:17 +0600

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
-test
+// select elements & assign them to variables
+let newTask = document.querySelector('#new-task');
+let form = document.querySelector('form');
+let todoUl = document.querySelector('#items');
+let completeUl = document.querySelector('.complete-list ul');
 
-test
 
-test
+// functions
+let createTask = function(task) {
+   let listItem = document.createElement('li');
+   let checkBox = document.createElement('input');
+   let label = document.createElement('label');
+
+   label.innerText = task;
+   checkBox.type = 'checkbox';
+
+   listItem.appendChild(checkBox);
+   listItem.appendChild(label);
+
+   return listItem;
+}
+
+let addTask = function(event) {
+   event.preventDefault();
+   let listItem = createTask(newTask.value);
+   todoUl.appendChild(listItem);
+   newTask.value = "";
+   // bind the new list item to the incomplete list
+   bindInCompleteItems(listItem, completeTask);
+}
+
+let completeTask = function() {
+   let listItem = this.parentNode;
+   let deleteBtn = document.createElement('button');
+   deleteBtn.innerText = 'Delete';
+   deleteBtn.className = 'delete';
+   listItem.appendChild(deleteBtn);
+
+   let checkBox = listItem.querySelector('input[type="checkbox"]');
+   checkBox.remove();
+   completeUl.appendChild(listItem);
+   bindCompleteItems(listItem, deleteTask);
+}
+
+let deleteTask = function() {
+   let listItem = this.parentNode;
+   let ul = listItem.parentNode;
+   ul.removeChild(listItem);
+}
+
+let bindInCompleteItems = function(taskItem, checkboxClick) {
+   let checkBox = taskItem.querySelector('input[type="checkbox"]');
+   checkBox.onchange = checkboxClick;
+}
+
+let bindCompleteItems = function(taskItem, deleteButtonClick) {
+   let deleteButton = taskItem.querySelector('.delete');
+   deleteButton.onclick = deleteButtonClick;
+}
+
+form.addEventListener('submit', addTask);
